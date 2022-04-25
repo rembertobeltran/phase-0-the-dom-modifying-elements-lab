c00365b09c74bedb926bc7325e4c489b7a6c2859
Remberto <rembertobeltran@icloud.com>
2022-04-24 20:34:06 -0400

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const removElement = document.getElementById("main");
+removElement.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "Remy is the champion.";
